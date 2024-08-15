5397c683434668a98a9e2d478135cc8aa63c2cf2
devil12820 <152226726+devil12820@users.noreply.github.com>
2024-08-15 16:36:41 +0530

    Update play.py

--- AviaxMusic/utils/inline/play.py
+++ AviaxMusic/utils/inline/play.py
@@ -64,9 +64,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
-            ),
-            InlineKeyboardButton( 
-                text="0 0000 0", url-f"https://t.me/Anime_Group_AGB"
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -82,12 +79,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton( 
-                text="0 0000 0", url-f"https://t.me/BRANDED WORLD"
-            ),
-        ],   
+        ], 
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
